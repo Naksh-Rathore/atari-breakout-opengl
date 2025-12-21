1aed17a07bb2f6eddafc187e2ae60ed03d6f32fd
Naksh-Rathore <naksh.rathore786@gmail.com>
2025-12-21 14:21:15 -0600

    Space out game.h better for readability

--- include/game/game.h
+++ include/game/game.h
@@ -8,12 +8,9 @@
 #include <game_objects/ball.h>
 #include <game_objects/paddle.h>
 
-
-
 #include <glm/glm.hpp>
 #include <glm/gtc/matrix_transform.hpp>
 #include <glm/gtc/type_ptr.hpp>
-
 
 namespace Game {
     class Game {
